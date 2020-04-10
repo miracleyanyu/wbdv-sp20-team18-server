3dd1b39de8ecf4b22d9ecc7dbf80d22be1b221c9
Yu Yan <miracleyanyu@gmail.com>
2020-04-10 11:16:45 -0700

    Update AdminUser.java

--- myapp/src/main/java/com/example/myapp/models/AdminUser.java
+++ myapp/src/main/java/com/example/myapp/models/AdminUser.java
@@ -1,30 +1,30 @@
-package com.example.myapp.models;
-
-import javax.persistence.*;
-
-@Entity
-@Table(name = "AdminUsers")
-public class AdminUser extends User {
-
-  private AdminLevel adminLevel;
-
-  public AdminUser(Integer userId, String userName, String firstName, String lastName, String password, String phone, String email,
-      com.example.myapp.models.Address address, AdminLevel adminLevel) {
-    super(userId, userName, firstName, lastName, password, phone, email, address);
-    this.adminLevel = adminLevel;
-  }
-
-  public AdminUser(String userName, String firstName, String lastName, String password, String phone, String email,
-      com.example.myapp.models.Address address, AdminLevel adminLevel) {
-    super(userName, password, firstName, lastName, phone, email, address);
-    this.adminLevel = adminLevel;
-  }
-
-  public AdminLevel getAdminLevel() {
-    return adminLevel;
-  }
-
-  public void setAdminLevel(AdminLevel adminLevel) {
-    this.adminLevel = adminLevel;
-  }
-}
+//package com.example.myapp.models;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "AdminUsers")
+//public class AdminUser extends User {
+//
+//  private AdminLevel adminLevel;
+//
+//  public AdminUser(Integer userId, String userName, String firstName, String lastName, String password, String phone, String email,
+//      com.example.myapp.models.Address address, AdminLevel adminLevel) {
+//    super(userId, userName, firstName, lastName, password, phone, email, address);
+//    this.adminLevel = adminLevel;
+//  }
+//
+//  public AdminUser(String userName, String firstName, String lastName, String password, String phone, String email,
+//      com.example.myapp.models.Address address, AdminLevel adminLevel) {
+//    super(userName, password, firstName, lastName, phone, email, address);
+//    this.adminLevel = adminLevel;
+//  }
+//
+//  public AdminLevel getAdminLevel() {
+//    return adminLevel;
+//  }
+//
+//  public void setAdminLevel(AdminLevel adminLevel) {
+//    this.adminLevel = adminLevel;
+//  }
+//}
